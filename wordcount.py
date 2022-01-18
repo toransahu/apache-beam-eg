19cedafe835455292cf562ffd12fec29d49a9df9
Toran Sahu <toran.sahu@yahoo.com>
2022-01-18 13:21:15 +0530

    initial commit

---
+++ wordcount.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2022-01-16 16:18
+
+"""Wordcount."""
+
+__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license'
+
+import argparse
+import logging
+import re
+
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+class WordExtractingDoFn(beam.DoFn):
+    """Parse each line of input text into words."""
+
+    def process(self, element):
+        """Returns an iterator over the words of this element.
+
+        The element is a line of text.  If the line is blank, note that, too.
+
+        Args:
+          element: the element being processed
+
+        Returns:
+          The processed element.
+        """
+        return re.findall(r'[\w\']+', element, re.UNICODE)
+
+
+def run(argv=None):
+    """Main entry point; defines and runs the wordcount pipeline."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        default='gs://dataflow-samples/shakespeare/kinglear.txt',
+        help='Input file to process.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=True,
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    # create a pipeline option
+    pipeline_options = PipelineOptions(pipeline_args)
+
+    # The pipeline will be run on exiting the with block.
+    with beam.Pipeline(options=pipeline_options) as p:
+
+        # Read the text file[pattern] into a PCollection.
+        lines = p | 'Read' >> ReadFromText(known_args.input)
+
+        counts = (
+            lines
+            | 'Split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(str))
+            # | 'LowerCaseWay1' >> beam.Map(lambda x: str.lower(x))
+            # | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+
+            | 'LowerCaseWay2' >> beam.Map(str.lower)
+            | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+
+            # | 'PairLowerCaseWithOne' >> beam.Map(lambda x: (x.lower(), 1))
+            | 'GroupAndSum' >> beam.CombinePerKey(sum))
+
+        # Format the counts into a PCollection of strings.
+        def format_result(word, count):
+            return '%s: %d' % (word, count)
+
+        output = counts | 'Format' >> beam.MapTuple(format_result)
+
+        # Write the output using a "Write" transform that has side effects.
+        # pylint: disable=expression-not-assigned
+        output | 'Write' >> WriteToText(known_args.output)
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()

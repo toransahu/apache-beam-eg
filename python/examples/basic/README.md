3a519cf0879097ef058a04b7a1697dc8c04755e4
Toran Sahu <toran.sahu@yahoo.com>
2022-03-30 09:26:44 +0530

    basic example

---
+++ python/examples/basic/README.md
@@ -0,0 +1,18 @@
+# Run local source code using DirectRunner
+
+```bash
+/github.com/toransahu/apache-beam-eg
+$ python python/examples/basic/etl.py \
+--runner DirectRunner
+```
+
+# Run local source code using Dataflow
+
+```bash
+/github.com/toransahu/apache-beam-eg
+$ python python/examples/basic/etl.py \
+--runner DataflowRunner \
+--region us-central1 \
+--project apache-beam-eg \
+--temp_location gs://apache-beam-eg/tmp
+```
---
+++ python/examples/basic/etl.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Etl."""
+
+import argparse
+from pathlib import Path
+import logging
+import re
+
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+this_dir = Path(__file__).resolve().parent
+
+
+class ETL:
+    def run(self, args=None):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--input',
+            # default='gs://dataflow-samples/shakespeare/kinglear.txt',
+            default=str(Path.joinpath(
+                this_dir.parent.parent.parent, 'data/small.txt')),
+            help='Input file to process.',
+        )
+        parser.add_argument(
+            '--output',
+            default=str(Path.joinpath(
+                this_dir.parent.parent.parent, 'results/python/examples/basic/output')),
+            help='Output file to write results to.',
+        )
+
+        known_args, extra_args = parser.parse_known_args(args)
+        pipeline_options = PipelineOptions(extra_args, save_main_session=True)
+        with beam.Pipeline(options=pipeline_options) as pipeline:
+            lines = (
+                pipeline
+                | "Read" >> beam.io.ReadFromText(known_args.input)
+            )
+            counts = (
+                lines
+                | 'Split' >> beam.ParDo(WordExtractingDoFn()).with_output_types(str)
+                | 'PairLowerCaseWithOne' >> beam.Map(lambda x: (x.lower(), 1))
+                | 'GroupAndSum' >> beam.CombinePerKey(sum)
+            )
+
+            # Format the counts into a PCollection of strings.
+            def format_result(word, count):
+                return '%s: %d' % (word, count)
+
+            output = counts | 'Format' >> beam.MapTuple(format_result)
+            # Write the output using a "Write" transform
+            output | 'Write' >> beam.io.WriteToText(known_args.output)
+
+
+class WordExtractingDoFn(beam.DoFn):
+    """Parse each line of input text into words."""
+
+    def process(self, element):
+        """Returns an iterator over the words of this element."""
+        return re.findall(r'[\w\']+', element, re.UNICODE)
+
+
+if __name__ == "__main__":
+    pipeline = ETL()
+    pipeline.run()
